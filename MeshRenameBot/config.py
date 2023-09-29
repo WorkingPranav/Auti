c1d1681299ae960ed19f6b013e41e74f4f86b73b
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-09-29 20:16:01 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "postgres://vyjeeprz:QZJFgX_Q5WXo81HDWoDMnYgMz2nvfRQW@snuffleupagus.db.elephantsql.com/vyjeeprz"]
+        API_HASH = [str, "c0da9c346d2c45dbc7ec49a05da9b2b6"]
+        API_ID = [int, 13675555]
+        BOT_TOKEN = [str, "6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
