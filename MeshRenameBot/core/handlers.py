0c55f9647062848c6710c5aa5c62107a35502caf
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-21 17:17:09 +0530

    Update handlers.py

--- MeshRenameBot/core/handlers.py
+++ MeshRenameBot/core/handlers.py
@@ -48,30 +48,32 @@ def add_handlers(client: Client) -> None:
 
 
 async def start_handler(client: Client, msg: Message) -> None:
-    await msg.reply(Trans.START_MSG, reply_markup=InlineKeyboardMarkup([
-    [
-        InlineKeyboardButton('⋆Oᴡɴᴇʀ⋆', url="https://t.me/BIackHatDev"),
-        InlineKeyboardButton('⋆Hᴇʟᴘ⋆', callback_data="help_str")
-    ]
-]))
+    start_message = Trans.START_MSG
+    keyboard = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton('⋆Oᴡɴᴇʀ⋆', url="https://t.me/BIackHatDev"),
+            InlineKeyboardButton('⋆Hᴇʟᴘ⋆', callback_data="help_str")
+        ]
+    ])
+    await msg.reply(start_message, reply_markup=keyboard)
 
 @Client.on_callback_query()
 async def help_callback(client, callback_query):
     if callback_query.data == "help_str":
-        # Edit the message with the help text
-        await callback_query.message.edit_text("""
-`/start` - Check if the bot is running.
-`/rename` - Reply to media to rename `/rename filename.extension`. If only `/rename` is used filters will be used.
-`/filters` - Add/Remove Filters. Use this command to see what are these.
-`/setthumb` - Reply to image to set the thumbnail permanently.
-`/getthumb` - Get the thumbnail which is currently set.
-`/clrthumb` - Remove the thumbnail which is set.
-`/mode` - Change between 3 modes:-
-    - Same format as it was sent. [If doc is sent doc is uploaded if video is sent video is uploaded.]
-    - Force to Document. [Everything is uploaded as a file.]
-    - Upload general media. [In streamable video/audio. etc.]
-`/queue` - Gives the state of your rename and the load on bot.
-    """, reply_markup=None)
+        help_text = """
+        `/start` - Check if the bot is running.
+        `/rename` - Reply to media to rename. Use `/rename filename.extension`. If only `/rename` is used, filters will be applied.
+        `/filters` - Add/Remove Filters. Use this command to see available filters.
+        `/setthumb` - Reply to an image to set the thumbnail permanently.
+        `/getthumb` - Get the current thumbnail.
+        `/clrthumb` - Remove the current thumbnail.
+        `/mode` - Change between 3 modes:
+            - Same format as it was sent. (If a document is sent, a document is uploaded. If a video is sent, a video is uploaded.)
+            - Force to Document. (Everything is uploaded as a file.)
+            - Upload general media. (In streamable video/audio, etc.)
+        `/queue` - Get the status of your rename and the bot's workload.
+        """
+        await callback_query.message.edit_text(help_text, reply_markup=None)
         
 
 
